65e7e4c2ce0042a92ed0194e5a7c3f81b4e0079d
Ivan Biriuk <ivan.biriuk@burberry.com>
2023-10-26 16:38:10 +0300

    add header folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.scss";
-import Button from "./components/button";
+
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="font-bold">IOM</h1>
-        <p>hello</p>
-        <Button primary >MY btn</Button>
-      </header>
+      <Header />
     </div>
   );
 }
