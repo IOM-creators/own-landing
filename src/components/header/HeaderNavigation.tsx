1f4d98535bab3c7cf5cd1f1a85297d1d20458973
Ivan Biriuk <ivan.biriuk@burberry.com>
2023-11-03 14:56:43 +0200

    refactore

--- src/components/header/HeaderNavigation.tsx
+++ src/components/header/HeaderNavigation.tsx
@@ -1,18 +1,10 @@
-import React, { useRef } from "react";
+import React from "react";
 import { IHeaderNavigation } from "../../helpers/commonInterfaces";
 
 const HeaderNavigation: React.FC<IHeaderNavigation> = ({
   classname = "",
   navigation,
-  setOpenNavChange = () => false,
 }) => {
-  const sectionRef = useRef<HTMLDivElement | null>(null);
-  const scrollTo = (e: any, navLink: string) => {
-    e.preventDefault();
-    console.log("sectionRef", navLink);
-    sectionRef.current &&
-      sectionRef.current.scrollIntoView({ behavior: "smooth" });
-  };
   return (
     <nav className={classname}>
       <ul className="flex h-full flex-col flex-wrap items-end lg:items-center lg:flex-row  lg:justify-center">
@@ -22,8 +14,9 @@ const HeaderNavigation: React.FC<IHeaderNavigation> = ({
             return (
               <li
                 key={index}
-                className={`text-white my-3 md:my-5 ${index === navigation.length - 1 ? "pr-0" : "pr-0 lg:pr-5 "
-                  } lg:my-0 lg:mb-0`}
+                className={`text-white my-3 md:my-5 ${
+                  index === navigation.length - 1 ? "pr-0" : "pr-0 lg:pr-5 "
+                } lg:my-0 lg:mb-0`}
               >
                 <a
                   href={`#${navLink}`}
