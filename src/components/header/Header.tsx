f3898b020aab7bd45918c531973091d0eddeceb0
Ivan Biriuk <birukivan1@gmail.com>
2024-02-01 13:00:45 +0200

    fixed bug

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,31 +5,18 @@ import Icon from "../icon";
 import HeaderNavigation from "./HeaderNavigation";
 import { useGetHeader } from "../../graphql/";
 import Button from "../button";
-import { useEffect } from "react";
 
 interface IHeader {}
 
 const Header: React.FC<IHeader> = () => {
   const { isHeaderVisible, activeLink, transparent } = useScrollEvent();
   const { header } = useGetHeader();
-  const hash = window.location.hash;
-  const sectionElem = document.querySelector(`${hash}`);
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
-
-  useEffect(() => {
-    if (hash && sectionElem) {
-      sectionElem.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const windowWidth = useWindowWidth();
   return (
