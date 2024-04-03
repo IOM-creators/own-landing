06dbc5f0004b4ee961fe7dc435668deb3f6f785b
Ivan Biriuk <birukivan1@gmail.com>
2024-04-03 14:47:25 +0300

    update blogs template

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,12 +1,18 @@
-import { NextPage } from "next/types"
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Section from "@/components/section";
 
+const Post = () => {
+  const router = useRouter();
+  const { slug } = router.query;
 
+  return (
+    <Section className="section py-10 my-10 md:py-16 md:my-16 container mx-lg">
+      <Link href="/blogs"> {"< Back to articles"} </Link>
+      <h1 className="text-2xl">Article: {slug}</h1>
+      {/* Fetch content based on slug */}
+    </Section>
+  );
+};
 
-const ArtilcleSlugPage: NextPage = () => {
-    return <>
-        <h1>Article</h1>
-    </>
-  }
-  
-
-export default ArtilcleSlugPage
+export default Post;
--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,12 +1,23 @@
-import { NextPage } from "next/types"
-
-
+import Section from "@/components/section";
+import Link from "next/link";
+import { NextPage } from "next/types";
 
 const ArtilcleSlugPage: NextPage = () => {
-    return <>
-        <h1>Article</h1>
-    </>
-  }
-  
+  return (
+    <Section className="section py-10 my-10 md:py-16 md:my-16 container mx-lg">
+      <h1 className="text-2xl">Articles</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/blog/test">Article test</Link>
+          </li>
+          <li>
+            <Link href="/blog/test2">Article test 2</Link>
+          </li>
+        </ul>
+      </nav>
+    </Section>
+  );
+};
 
-export default ArtilcleSlugPage
+export default ArtilcleSlugPage;
