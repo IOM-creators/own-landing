67fb447af0f35d432c2042ed866886d782309eac
Oleh <oleg.statkevych@bigdropinc.com>
2024-04-08 19:20:53 +0300

    update blogs page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,15 +1,17 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Section from "@/components/section";
+import { useProject } from "@/graphql/queries/blogs";
 
 const Post = () => {
   const router = useRouter();
   const { slug } = router.query;
-
+  const { content } = useProject(slug as string);
   return (
     <Section className="section py-10 my-10 md:py-16 md:my-16 container mx-lg">
       <Link href="/blogs"> {"< Back to articles"} </Link>
-      <h1 className="text-2xl">Article: {slug}</h1>
+      <h1 className="text-2xl"> Article : {content && content.item && content.item.title}</h1>
+      <h2> Slug : {content && content.item && content.item.slug}</h2>
       {/* Fetch content based on slug */}
     </Section>
   );
--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,22 +1,25 @@
 import Section from "@/components/section";
 import Link from "next/link";
 import { NextPage } from "next/types";
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import { useProjects } from "@/graphql/queries/blogs";
+import { useEffect, useMemo, useState } from "react";
+
 
 const ArtilcleSlugPage: NextPage = () => {
+
+  const { content } = useProjects();
+
   return (
     <Section className="section py-10 my-10 md:py-16 md:my-16 container mx-lg">
       <h1 className="text-2xl">Articles</h1>
       <nav>
         <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/blog/test">Article test</Link>
-          </li>
-          <li>
-            <Link href="/blog/test2">Article test 2</Link>
-          </li>
+          {content && content.items && content.items.map((item:any, index:number) => {
+            return <li key={item.slug}>
+              <Link href={`/blog/${item.slug}`}>{index + 1}: {item.title}</Link>
+            </li>
+          })}
         </ul>
       </nav>
     </Section>
