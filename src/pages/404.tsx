594e2ea9edb9f6916fd279a64d450c3493888485
Ivan Biriuk <birukivan1@gmail.com>
2024-04-26 15:57:52 +0300

    refactore

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,8 +2,8 @@ import Section from "@/components/section";
 
 export default function Custom404() {
   return (
-    <Section className="section container h-full flex items-center justify-center">
+    <section className="section container h-full flex items-center justify-center">
       <h1>404 - Page Not Found</h1>
-    </Section>
+    </section>
   );
 }
