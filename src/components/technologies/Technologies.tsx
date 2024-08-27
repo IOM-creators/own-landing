44e002da02cd2f23c72b052924d5fe5708c83fe1
Ivan Biriuk <i.biriuk@viseven.com>
2024-08-27 13:08:43 +0300

    update dechnologies

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -20,8 +20,10 @@ const Technologies: React.FC<ITechnologies> = ({ id = "", section }) => {
             key={index}
             className="inline-block p-6 md:p-10 text-center max-w-32 md:max-w-40 w-full mr-6 md:mr-10"
           >
-            {item.icon && <Image src={item.icon.url} />}
-            {item.title && <span>{item.title}</span>}
+            <div className="flex flex-col gap-4">
+              {item.icon && <Image src={item.icon.url} />}
+              {item.title && <span>{item.title}</span>}
+            </div>
           </div>
         ))}
       </Marquee>
