3b13d3d968e2970e0a098d5bfc5a8a412bcee3c2
Ivan Biriuk <i.biriuk@viseven.com>
2024-09-07 18:03:02 +0300

    updated seo component

--- src/components/seo/Seo.tsx
+++ src/components/seo/Seo.tsx
@@ -1,71 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
-import siteImage from "../../assets/images/about_us.png";
+
 interface ISeo {
-  title: string;
-  description: string;
-  imagePath: string;
-  siteUrl: string;
-  pageKeywords: string;
+  title?: string;
+  description?: string;
+  imagePath?: string;
+  pageKeywords?: string;
 }
 
-// TODO
-
-const seoData: ISeo = {
+const defaultSeoData: ISeo = {
   title: "IOM Creators",
   description:
     "Explore stunning designs & seamless e-commerce. Discover our crafted websites, optimized for performance & engagement. Elevate your online presence with IOM Creators' custom e-commerce solutions.",
   pageKeywords: `
-      iom,
-      iom creators,
-      iom-creators,
-      iom shopify,
-      rontend development,
-      responsive web design,
-      user interface (UI) design,
-      user experience (UX) design,
-      HTML5, CSS3, JavaScript,
-      e-commerce development,
-      online store development,
-      e-commerce website design,
-      e-commerce solutions,
-      custom e-commerce development,
-      shopping cart integration,
-      Shopify,
-      Shopify development,
-      Shopify theme development,
-      Shopify app development,
-      Shopify customization,
-      Shopify store setup,
-      Shopify Plus development,
-      front-end frameworks (e.g., React, Angular, Vue),
-      CSS preprocessors (e.g., Sass, Less),
-      JavaScript frameworks/libraries,
-      responsive design,
-      mobile-friendly websites,
-      online business development,
-      digital storefront,
-      e-commerce solutions provider,
-      Shopify agency
+    iom,
+    iom creators,
+    iom-creators,
+    iom shopify,
+    frontend development,
+    responsive web design,
+    e-commerce development,
+    Shopify development,
+    wordpress,
+    iom develop,
+    custom e-commerce solutions
   `,
-  imagePath: `${window.location.origin + siteImage}`,
-  siteUrl: `${window.location.origin}`,
 };
 
-const Seo = () => {
+const Seo: React.FC<ISeo> = ({
+  title,
+  description,
+  imagePath,
+  pageKeywords,
+}) => {
+  const [currentUrl, setCurrentUrl] = useState("");
+
+  // Set URL only on the client side
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setCurrentUrl(window.location.href);
+    }
+  }, []);
+
+  const seoData: ISeo = {
+    title: title || defaultSeoData.title,
+    description: description || defaultSeoData.description,
+    pageKeywords: pageKeywords || defaultSeoData.pageKeywords,
+    imagePath: imagePath || `${currentUrl}/assets/images/iom.png`,
+  };
+
   return (
     <Helmet>
-
+      <title>{seoData.title}</title>
+      <meta name="description" content={seoData.description} />
+      <meta name="keywords" content={seoData.pageKeywords} />
       <meta name="robots" content="index, follow" />
 
-      {/* Tags for social services  */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={seoData.siteUrl} />
+      <meta property="og:url" content={currentUrl} />
       <meta property="og:title" content={seoData.title} />
       <meta property="og:description" content={seoData.description} />
       <meta property="og:image" content={seoData.imagePath} />
 
-      <meta name="twitter:site" content={seoData.siteUrl} />
+      <meta name="twitter:site" content={currentUrl} />
       <meta name="twitter:title" content={seoData.title} />
       <meta name="twitter:description" content={seoData.description} />
       <meta name="twitter:image" content={seoData.imagePath} />
