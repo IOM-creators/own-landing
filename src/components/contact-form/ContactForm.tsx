b1572cf314c77f5ec402cc69ba36fabec0135d6f
Ivan Biriuk <i.biriuk@viseven.com>
2024-09-18 23:11:20 +0300

    update animation

--- src/components/contact-form/ContactForm.tsx
+++ src/components/contact-form/ContactForm.tsx
@@ -114,16 +114,13 @@ const ContactForm: React.FC<IContactUs> = ({
       className="contact-form max-w-[710px] px-4 py-8 lg:p-[80px] border-contact-form bg-white  w-full mx-auto relative"
       style={customStyles}
     >
-      <div className="overflow-hidden">
+      <>
         {contactForm?.title && (
           <h3
-            className={cn(
-              {
-                "mb-[40px]": !contactForm.subtitle,
-                "mb-2": contactForm.subtitle,
-              },
-              " overflow-hidden"
-            )}
+            className={cn({
+              "mb-[40px]": !contactForm.subtitle,
+              "mb-2": contactForm.subtitle,
+            })}
             data-animate="moveRight"
           >
             {contactForm?.title}
@@ -226,7 +223,7 @@ const ContactForm: React.FC<IContactUs> = ({
             <RichText richText={contactForm.successMessage.json} />
           </div>
         )}
-      </div>
+      </>
     </div>
   );
 };
