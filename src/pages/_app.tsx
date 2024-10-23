3dfbb30e065a7459109ed43a9f856c7610e82827
Ivan Biriuk <i.biriuk@viseven.com>
2024-10-24 01:17:30 +0300

    remove animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,7 +44,7 @@ export default function App({ Component, pageProps }: AppProps) {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
-  useGlobalAnimationObserver();
+  // useGlobalAnimationObserver();
 
   return (
     <Provider store={store}>
