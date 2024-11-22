4bfec1db4718c6dc4c9f85a8ba8fba6adb95e9f6
IvanBiriuk <ivan.biriuk@iom-creators.com>
2024-11-22 13:37:01 +0200

    refactor

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -180,12 +180,13 @@ const Section: React.FC<ISection> = ({ id, className, section, tagH1 }) => {
               let ComponentGql = componentMap[updatedKey];
               return (
                 <motion.div
+                  key={index}
                   initial={{ opacity: 0, y: 30 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.5, delay: 0.3 * index }}
                   viewport={{ once: true, amount: 0.1 }}
                 >
-                  <ComponentGql section={{ ...component }} key={index} />{" "}
+                  <ComponentGql section={{ ...component }} />{" "}
                 </motion.div>
               );
             }
